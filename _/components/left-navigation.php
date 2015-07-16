b4faa7d72a9fbb2d85f2d2c98932f3164faeda0f
mbehn <matthew.behn@gmail.com>
2015-07-16 00:35:48 -0500

    added shows pages

--- _/components/left-navigation.php
+++ _/components/left-navigation.php
@@ -2,6 +2,7 @@
 	<ul id="left-navigation-menu-items">
 		<li class="nav-menu-item"><a href="index.php">Home</a></li>
 		<li class="nav-menu-item"><a href="about-us.php">About Us</a></li>
+		<li class="nav-menu-item"><a href="shows.php">Shows</a></li>
 		<li class="nav-menu-item"><a href="media.php">Media</a></li>
 		<li class="nav-menu-item"><a href="gallery.php">Gallery</a></li>
 		<li class="nav-menu-item"><a href="contact-us.php">Contact Us</a></li>
---
+++ shows.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Khali Haat Afrobeat Music - Home</title>
+	<meta name="keywords" content="Afrobeat, Austin, Austin TX, Music, Live Music, Dance, World Music, For Hire, Musicians">
+	<?php include "_/components/assets.php" ?>
+</head>
+<body id="shows">
+	<div class="container">
+		<?php include "_/components/header.php" ?>
+		<?php include "_/components/left-navigation.php" ?>
+	</div>
+<?php include "_/components/footer-assets.php" ?>
+</body>
+</html>
