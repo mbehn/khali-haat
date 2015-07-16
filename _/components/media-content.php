1509d7a6828d27051d13d9a045d89d71af8a95a7
mbehn <matthew.behn@gmail.com>
2015-07-15 23:51:46 -0500

    removed background in media gallery for now

--- _/components/media-content.php
+++ _/components/media-content.php
@@ -1,4 +1,4 @@
-<div class="main-content-area col-lg-8 ">
+<div class="main-content-area">
 	<div class="col-lg-8 offset2">
 		<iframe width="640" class="offset2" height="480" src="https://www.youtube.com/embed/3yur74v5EAw" frameborder="0" allowfullscreen></iframe>	
 	</div>
